8859cd7e044a83708cedd1a8565c2a4edfa537de
Mathieu de Maleingreau <mdemaleingreau@gmail.com>
2019-04-08 22:16:14 +0200

    shipping details

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -62,5 +62,10 @@ namespace SportsStore.WebUI.Controllers
         {
             return PartialView(cart);
         }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
